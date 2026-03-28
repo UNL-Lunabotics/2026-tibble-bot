353d5d0fa016b194a37834350882c605bf6ccbdc
TheThingKnownAsKit <moodyellam@gmail.com>
2026-03-28 02:26:49 -0500

    im so fucking tired man

--- teensy_firmware/travel_test.cpp
+++ teensy_firmware/travel_test.cpp
@@ -4,22 +4,70 @@
 
 #define ROBOCLAW_BAUD 115200
 
-#define ROBOCLAW_ADDRESS_1 0x82   // Linear Actuators
-#define ROBOCLAW_ADDRESS_2 0x83   // Vibe and Excav
-#define HOPPER_SERVO_PIN 7  // placeholder
-#define KRAKEN_RIGHT_PIN 8  // placeholder
-#define KRAKEN_LEFT_PIN 9   // placeholder
+#define ROBOCLAW_ADDRESS_1 0x83   // Linear Actuators
+#define ROBOCLAW_ADDRESS_2 0x82   // Vibe and Excav
+#define HOPPER_SERVO_PIN 9  // placeholder
+#define KRAKEN_RIGHT_PIN 13  // placeholder
+#define KRAKEN_LEFT_PIN 14   // placeholder
 
 // bullshit PWM values
-const unsigned int LA_POS = 41100.0 * 0.00762;  // ticks/meter * 0.3 inches in meters
+// const unsigned int LA_POS = 41100.0 * 0.00762;  // ticks/meter * 0.3 inches in meters
+const unsigned int LA_POS = 41100.0 * 0.0020;   // just to see if theyll move
 const unsigned int HOP_SERVO_POS = 180;
-const int VIBE_SPEED = 127;
-const int EXCAV_SPEED = 127;
-const int DRIVETRAIN_SPEED = 127;
+const int VIBE_SPEED = 64;
+const int EXCAV_SPEED = 64;
+const int DRIVETRAIN_SPEED = 110;
 
-unsigned int time_limit = 30;
+unsigned int time_limit = 60 * 1000; // seconds
 
 RoboClaw roboclaw(&Serial2, 10000); 
 Servo hopper_latch;
 Servo kraken_right;
 Servo kraken_left;
+
+void setup() {
+    roboclaw.begin(ROBOCLAW_BAUD);
+
+    hopper_latch.attach(HOPPER_SERVO_PIN);
+
+    kraken_right.attach(KRAKEN_RIGHT_PIN);
+    kraken_left.attach(KRAKEN_LEFT_PIN);
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    
+    // Ensure all motors are STOPPED on boot
+    roboclaw.ForwardBackwardM1(ROBOCLAW_ADDRESS_1, 64);
+    roboclaw.ForwardBackwardM2(ROBOCLAW_ADDRESS_1, 64);
+    roboclaw.ForwardBackwardM1(ROBOCLAW_ADDRESS_2, 64);
+    roboclaw.ForwardBackwardM2(ROBOCLAW_ADDRESS_2, 64);
+    kraken_left.write(90);
+    kraken_right.write(90);
+
+    // Flash LED to show boot is complete
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(200);
+    digitalWrite(LED_BUILTIN, LOW);
+}
+
+// goal
+// drivetrain forward and LA's REST, vibe motor off, Latch closed, Excav off (travel)
+void loop() {
+    // if (millis() >= time_limit) {
+    if (false) {
+        // set everything to rest or stop motors
+        roboclaw.ForwardBackwardM1(ROBOCLAW_ADDRESS_1, 64);
+        roboclaw.ForwardBackwardM2(ROBOCLAW_ADDRESS_1, 64);
+        roboclaw.ForwardBackwardM1(ROBOCLAW_ADDRESS_2, 64);
+        roboclaw.ForwardBackwardM2(ROBOCLAW_ADDRESS_2, 64);
+        kraken_left.write(64);
+        kraken_right.write(64);
+    } else {
+        roboclaw.SpeedAccelDeccelPositionM1(ROBOCLAW_ADDRESS_1, 1000, 5000, 5000, LA_POS, 1);
+        roboclaw.SpeedAccelDeccelPositionM2(ROBOCLAW_ADDRESS_1, 1000, 5000, 5000, LA_POS, 1);
+        roboclaw.ForwardBackwardM1(ROBOCLAW_ADDRESS_2, VIBE_SPEED);
+        roboclaw.ForwardBackwardM2(ROBOCLAW_ADDRESS_2, EXCAV_SPEED);
+        kraken_left.write(DRIVETRAIN_SPEED);
+        kraken_right.write(DRIVETRAIN_SPEED);
+        hopper_latch.write(HOP_SERVO_POS);  // just close it
+    }
+}
