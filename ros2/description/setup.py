91e9875d2cfed199f8b3627ab65119ad657a59bc
TheKing349 <apkimmerling@gmail.com>
2026-04-04 12:14:24 -0500

    Re-add main urdf meshes

--- ros2/description/setup.py
+++ ros2/description/setup.py
@@ -13,6 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join("share", package_name, "urdf"), glob("urdf/*")),
+        (os.path.join("share", package_name, "meshes"), glob("meshes/*")),
         (os.path.join("share", package_name, "worlds"), glob("worlds/*.sdf")),
         (os.path.join("share", package_name, "worlds/meshes/ksc"), glob("worlds/meshes/ksc/*")),
         (os.path.join("share", package_name, "worlds/meshes/rocks"), glob("worlds/meshes/rocks/*")),
