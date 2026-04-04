33f00c53c0331bb431de7e1c4723d5da274ef1e4
TheKing349 <apkimmerling@gmail.com>
2026-04-04 11:50:39 -0500

    Add all paths in worlds file

--- ros2/description/setup.py
+++ ros2/description/setup.py
@@ -13,7 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join("share", package_name, "urdf"), glob("urdf/*")),
-        (os.path.join("share", package_name, "meshes"), glob("meshes/*")),
+        (os.path.join("share", package_name, "worlds"), glob("worlds/*/*/*/*")),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
