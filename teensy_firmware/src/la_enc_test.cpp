f80dc0a9d703430981028469cae3d45549c33b6f
TheThingKnownAsKit <moodyellam@gmail.com>
2026-04-08 20:26:06 -0500

    made the la test code not terrible :)

--- teensy_firmware/src/la_enc_test.cpp
+++ teensy_firmware/src/la_enc_test.cpp
@@ -7,12 +7,15 @@
 
 #define ROBOCLAW_ADDRESS_1 0x82   // Linear Actuators
 
-const unsigned int LA_SPEED = 1;   // just to see if theyll move - 64 is stop, set to 1 for min
+const unsigned int LA_SPEED = 10000;   // just to see if theyll move - 64 is stop, set to 1 for min
 const unsigned int LA_POS = 15000;
 
 unsigned int time_limit = 60 * 1000; // seconds
 
 RoboClaw roboclaw_1(&Serial1, 10000); 
+
+bool move_commanded = false;
+unsigned long last_print_time = 0;
 
 void setup() {
     roboclaw_1.begin(ROBOCLAW_BAUD);
@@ -28,24 +31,26 @@ void setup() {
     digitalWrite(LED_BUILTIN, HIGH);
     delay(200);
     digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
 }
 
-// goal
-// drivetrain forward and LA's EXCAVATE, vibe motor on, Latch closed, Excav on (excav)
 void loop() {
-    // if (millis() >= time_limit) {
-    if (false) {
-        // set everything to rest or stop motors
-        roboclaw_1.ForwardBackwardM1(ROBOCLAW_ADDRESS_1, 64);
-        roboclaw_1.ForwardBackwardM2(ROBOCLAW_ADDRESS_1, 64);
-    } else {
-        roboclaw_1.SpeedAccelDeccelPositionM1(ROBOCLAW_ADDRESS_1, 10000, 10000, 10000, LA_POS, 1);
-        roboclaw_1.SpeedAccelDeccelPositionM2(ROBOCLAW_ADDRESS_1, 10000, 10000, 10000, LA_POS, 1);
+    if (!move_commanded) {
+        roboclaw_1.SpeedAccelDeccelPositionM1(ROBOCLAW_ADDRESS_1, LA_SPEED, LA_SPEED, LA_SPEED, LA_POS, 0);
+        roboclaw_1.SpeedAccelDeccelPositionM2(ROBOCLAW_ADDRESS_1, LA_SPEED, LA_SPEED, LA_SPEED, LA_POS, 0);
+        
+        move_commanded = true;
+    }
+
+    if (millis() - last_print_time >= 100) {
+        last_print_time = millis();
 
         bool v1, v2;
-        uint8_t status;
-        int32_t enc_la1 = roboclaw_1.ReadEncM1(ROBOCLAW_ADDRESS_1, &status, &v1);
-        int32_t enc_la2 = roboclaw_1.ReadEncM2(ROBOCLAW_ADDRESS_1, &status, &v2);
-        Serial.printf("ENC1: %d, ENC2: %d", enc_la1, enc_la2);
+        uint8_t status1, status2; 
+        
+        int32_t enc_la1 = roboclaw_1.ReadEncM1(ROBOCLAW_ADDRESS_1, &status1, &v1);
+        int32_t enc_la2 = roboclaw_1.ReadEncM2(ROBOCLAW_ADDRESS_1, &status2, &v2);
+        
+        Serial.printf("ENC1: %d, ENC2: %d\n", enc_la1, enc_la2);
     }
 }
